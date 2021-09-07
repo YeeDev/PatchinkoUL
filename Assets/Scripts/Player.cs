5dfa989fb8fcaf407d109367409a2d2bd49462ee
Yee <yee.gamedev@gmail.com>
2021-09-06 23:54:14 -0500

    Movement Implemented

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 0.1f;
+
+    bool facingLeft;
+    float xAxis;
+    Animator anm;
+    Rigidbody2D rg2D;
+
+    private void Awake()
+    {
+        anm = GetComponent<Animator>();
+        rg2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        xAxis = Input.GetAxisRaw("Horizontal");
+        anm.SetBool("IsWalking", Mathf.Abs(xAxis) > 0);
+
+        CheckIfFlip();
+    }
+
+    private void FixedUpdate()
+    {
+        rg2D.MovePosition(transform.position + Vector3.right * xAxis * moveSpeed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.CompareTag("Apple"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+
+    private void CheckIfFlip()
+    {
+        if (xAxis > 0 && facingLeft)
+        {
+            FlipSprite();
+        }
+        else if (xAxis < 0 && !facingLeft)
+        {
+            FlipSprite();
+        }
+    }
+
+    private void FlipSprite()
+    {
+        facingLeft = !facingLeft; 
+        Vector3 flipScale = transform.localScale;
+        flipScale.x *= -1;
+        transform.localScale = flipScale;
+    }
+}
